8c3c9549c025798a7331c7d1a8bc72ebd5a8bc58
Aaron Addleman <aaronaddleman@gmail.com>
2020-12-22 09:17:41 -0800

    make test pass

--- tdd/ds/ds/stack.py
+++ tdd/ds/ds/stack.py
@@ -1,2 +1,8 @@
 class Stack:
-    pass
+    def __init__(self):
+        # internal storage of data structure
+        # using a _ to mark it as private data
+        self._storage = []
+
+    def __len__(self):
+        return len(self._storage)
